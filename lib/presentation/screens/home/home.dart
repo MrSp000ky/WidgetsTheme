8bcb7da1c82a47be94b5545ea28bf2d11cb56aa0
MrSp000ky <miguelcamacho241@outlook.com>
2024-03-20 23:21:24 -0700

    Commit 20 Marzo

--- lib/presentation/screens/home/home.dart
+++ lib/presentation/screens/home/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:widgets_2024_1/config/menu/menu_items.dart';
+import 'package:widgets_2024_1/presentation/widgets/CustomListTitle.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,9 +8,27 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Flutter + Material 3"),
-      
+      appBar: AppBar(title: const Text("Flutter + Material 3"), 
       ),
+      body: const _HomeView(),
     );
   }
 }
+
+class _HomeView extends StatelessWidget {
+  const _HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return ListView.builder
+    (
+      itemCount: appMenuItems.length, itemBuilder: getMenuList
+    );
+  }
+
+  Widget getMenuList (context,index)=>
+         CustomListTitle(menuItem: appMenuItems[index]);
+
+}
+
---
+++ lib/presentation/widgets/CustomListTitle.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:widgets_2024_1/config/menu/menu_items.dart';
+
+
+class CustomListTitle extends StatelessWidget {
+  const CustomListTitle({
+    super.key,
+    required this.menuItem,
+  });
+
+  final MenuItem menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(
+        menuItem.icon,
+        ),
+        trailing: const Icon(Icons.arrow_circle_right_outlined),
+      title: Text(menuItem.title),
+      subtitle: Text(menuItem.subtitle),
+      onTap: () => {
+        print("Llamado de la liga para ir a ${menuItem.title} en la liga de ${menuItem.link}")
+      },);
+  }
+}
