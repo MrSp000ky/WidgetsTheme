762c946342901b0b4dcc73afc00b7a21276ae698
MrSp000ky <miguelcamacho241@outlook.com>
2024-03-21 19:56:25 -0700

    Commit Tarea MenuItems

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -16,5 +16,26 @@ class MenuItem{
             link: "/button", icon: Icons.radio_button_on),
             
     MenuItem(title: "Tarjetas", 
-            subtitle: "Contenedor Estilizado", link: "/card", icon: Icons.card_giftcard_sharp)
+            subtitle: "Contenedor Estilizado", link: "/card", icon: Icons.card_giftcard_sharp),
+
+    MenuItem(title: "Progress Indicators", subtitle: "Generales y controlados", 
+            link: "/progress_indicators", icon: Icons.replay_outlined),
+
+    MenuItem(title: "Snackbar y Dialogos", subtitle: "Indicadores en pantalla", 
+            link: "/snackbar_dialogos", icon: Icons.perm_device_information),
+    
+    MenuItem(title: "Snackbar y Dialogos", subtitle: "Indicadores en pantalla", 
+            link: "/snackbar_dialogos_2", icon: Icons.square_sharp),
+    
+    MenuItem(title: "UI Control + Tiles", subtitle: "Una Serie de Controles de Flutter", 
+            link: "/UI_control", icon: Icons.car_rental),
+    
+    MenuItem(title: "Introduccion a la aplicacion", subtitle: "Pequeño tutorial introductorio", 
+            link: "/introduction", icon: Icons.handshake_outlined),
+
+    MenuItem(title: "Infinite Scroll y Pull", subtitle: "Listas Infinitas y Pull To Refresh", 
+            link: "/infinite_scroll_and_pull", icon: Icons.add_chart_outlined),
+    
+    MenuItem(title: "Cambiar Tema", subtitle: "Cambiar tema de la aplicacion", 
+            link: "/change_theme", icon: Icons.palette_rounded),
   ];
